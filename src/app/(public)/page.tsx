6dbd4d3b3140cb0f908081c4606c26cccadfb355
ccrsxx <aminrisal@gmail.com>
2023-11-08 22:41:19 +0700

    feat: change several routes

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,10 +2,10 @@
 
 import { useState } from 'react';
 import { useFormState } from 'react-dom';
-import { z } from 'zod';
-import { NEXT_PUBLIC_URL } from '@/lib/env';
+import { URL_WITHOUT_PROTOCOL } from '@/lib/env';
 import { Alert } from '@/components/ui/alert';
 import { createLink } from '../actions';
+import { validUrl } from '../schema';
 import { Submit } from './submit';
 import type { ChangeEvent } from 'react';
 
@@ -23,16 +23,18 @@ export default function Home(): JSX.Element {
     target: { value }
   }: ChangeEvent<HTMLInputElement>): void => setUrl(value);
 
-  const isValidURL = z.string().url().safeParse(url).success;
+  const isValidURL = validUrl.safeParse(url).success;
 
   return (
     <main>
       <section className='mx-auto w-full max-w-md'>
         <form className='grid gap-4' action={formAction}>
-          <div className='flex flex-col items-center gap-4 sm:flex-row'>
-            <p>{NEXT_PUBLIC_URL}/</p>
+          <div className='flex w-full items-center py-0 pr-0'>
+            <p className='custom-input rounded-r-none border-r-0 text-gray-600 dark:text-gray-300'>
+              {URL_WITHOUT_PROTOCOL}/
+            </p>
             <input
-              className='custom-input w-full'
+              className='custom-input w-full rounded-l-none'
               name='slug'
               type='text'
               value={slug}
--- src/app/(public)/s/[slug]/page.tsx
+++ src/app/(public)/s/[slug]/page.tsx
@@ -1,26 +1,26 @@
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
-import { checkSlugExists } from '@/lib/helper-server';
+import { checkSlugExists } from '@/lib/helper';
 import { NEXT_PUBLIC_URL } from '@/lib/env';
 import { CopyButton } from '@/components/ui/copy-button';
 import { Button } from '@/components/ui/button';
 
 export default async function Success({
-  searchParams: { slug }
+  params: { slug }
 }: {
-  searchParams: { slug: string };
+  params: { slug: string };
 }): Promise<JSX.Element> {
   const slugExists = await checkSlugExists(slug);
 
   if (!slugExists) redirect('/');
 
-  const url = `${NEXT_PUBLIC_URL}/${slug}`;
+  const url = `${NEXT_PUBLIC_URL}/l/${slug}`;
 
   return (
     <main>
       <section className='mx-auto grid w-full max-w-md gap-4'>
         <h1>Your short link has been created successfully:</h1>
-        <div className='main-border relative rounded p-4'>
+        <div className='main-border relative rounded px-3 py-2'>
           <a
             className='animated-underline'
             href={url}
