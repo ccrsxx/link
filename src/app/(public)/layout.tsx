4e327efd564abc166ffa94fb881941394a58bd29
ccrsxx <aminrisal@gmail.com>
2023-11-15 19:33:53 +0700

    feat: add seo to the project

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -9,7 +9,35 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Link',
-  description: 'Basic Link shortener'
+  description: 'A simple URL shortener.',
+  authors: { name: 'Risal Amin', url: 'https://risalamin.com' },
+  generator: 'Next.js',
+  openGraph: {
+    title: 'Link',
+    description: 'A simple URL shortener.',
+    determiner: 'auto',
+    url: 'https://link.risalamin.com',
+    locale: 'en_US',
+    siteName: 'risalamin.com',
+    images: {
+      url: 'https://risalamin.com/api/og?title=Link&description=A%20simple%20URL%20shortener.',
+      alt: 'Link',
+      type: 'image/png',
+      width: 1200,
+      height: 600
+    }
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@ccrsxx',
+    creator: '@ccrsxx',
+    title: 'Link',
+    description: 'A simple URL shortener.',
+    images: {
+      url: 'https://risalamin.com/api/og?title=Link&description=A%20simple%20URL%20shortener.',
+      alt: 'Link'
+    }
+  }
 };
 
 export default function RootLayout({
